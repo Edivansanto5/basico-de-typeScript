386e7e0348b1452e316655716780027472da96f5
Edivansanto5 <edivansantos1478@gmail.com>
2022-09-05 20:24:41 -0300

    finalizando a aula co o tipo de Array

--- Modulo02/typeAnotacion/app.js
+++ Modulo02/typeAnotacion/app.js
@@ -1,4 +1,11 @@
 "use strict";
+// typeAnnotacion do tipo string
 let nome;
 nome = 'Anne da silva lemos';
 console.log(nome);
+//typeAnootacion do tipo Array
+let animais = ['Macaco', 'Elefante', 'Sapo', 'Onça', 'Urso', 'Cobra', 'Panda'];
+console.log(animais);
+//typeAnnotacion do tipo objeto
+// typeAnnotacion do tipo function
+// 
--- Modulo02/typeAnotacion/app.ts
+++ Modulo02/typeAnotacion/app.ts
@@ -1,4 +1,20 @@
+// typeAnnotacion do tipo string
+
 let nome: string;
 nome = 'Anne da silva lemos'
 
 console.log(nome);
+
+//typeAnootacion do tipo Array
+
+let animais:string[ ] = ['Macaco','Elefante','Sapo','Onça','Urso','Cobra','Panda'];
+console.log(animais);
+
+
+//typeAnnotacion do tipo objeto
+
+
+// typeAnnotacion do tipo function
+
+
+// 
