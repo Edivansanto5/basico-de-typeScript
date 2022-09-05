c089e3d611e3b94acd1354685901b88a527c8020
Edivansanto5 <edivansantos1478@gmail.com>
2022-09-05 20:56:02 -0300

    finalizando a aula de instanciando um objeto

--- Modulo02/typeAnotacion/app.js
+++ Modulo02/typeAnotacion/app.js
@@ -10,5 +10,16 @@ console.log(animais);
 let pessoa;
 pessoa = { nome: 'Maria de ludes', idade: 33, sexo: 'Masculino', peso: 1.98 };
 console.log(pessoa);
-// typeAnnotacion do tipo function
+// criando outro objeto do tipo de carro
+let carro;
+console.log(' ');
+carro = { nome: 'sedan', cor: 'preto', anoDeLancamento: 2022 };
+console.log(carro);
+// typeAnnotacion do tipo function 
+function MediaNota(nota01, nota02, nota03, nota04) {
+    let somaNotas = nota01 + nota02 + nota03 + nota04;
+    let mediaNotas = somaNotas / 4;
+    console.log(mediaNotas);
+}
+MediaNota(6, 7, 9, 5);
 // 
--- Modulo02/typeAnotacion/app.ts
+++ Modulo02/typeAnotacion/app.ts
@@ -22,7 +22,26 @@ let pessoa:{
 pessoa={nome:'Maria de ludes',idade:33,sexo:'Masculino',peso:1.98}
 console.log(pessoa);
 
-// typeAnnotacion do tipo function
+// criando outro objeto do tipo de carro
+
+let carro:{
+    nome:string;
+    cor:string;
+    anoDeLancamento:number
+}
+console.log(' ');
+
+carro={nome:'sedan',cor:'preto',anoDeLancamento:2022}
+console.log(carro);
+
+// typeAnnotacion do tipo function 
+function MediaNota(nota01:number,nota02:number,nota03:number,nota04:number){
+    let somaNotas:number = nota01+ nota02+ nota03 + nota04;
+    let mediaNotas:number = somaNotas/4;
+
+    console.log(mediaNotas);
+}
+MediaNota(6,7,9,5)
 
 
 // 
